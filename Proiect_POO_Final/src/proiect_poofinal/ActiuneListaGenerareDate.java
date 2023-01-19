ae08c7a0ff84d270577f8c3098ab33daf5bba67a
vladvladut1124 <120597877+vladvladut1124@users.noreply.github.com>
2023-01-19 23:42:24 +0200

    Update ActiuneListaGenerareDate.java

---
+++ Proiect_POO_Final/src/proiect_poofinal/ActiuneListaGenerareDate.java
@@ -0,0 +1,21 @@
+package proiect_poofinal;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+
+public class ActiuneListaGenerareDate implements ActionListener{
+
+	public static ArrayList<Speaker> listaSpeaker;
+     
+
+    public void actionPerformed(ActionEvent e) {
+
+      
+    }
+  
+    
+    
+
+}
