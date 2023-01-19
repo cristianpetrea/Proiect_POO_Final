47d6069c01c586ff13610ba901ff48895f69138c
vladvladut1124 <120597877+vladvladut1124@users.noreply.github.com>
2023-01-19 23:41:57 +0200

    Update ActiuneListaFiltrare.java

---
+++ Proiect_POO_Final/src/proiect_poofinal/ActiuneListaFiltrare.java
@@ -0,0 +1,24 @@
+package proiect_poofinal;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+
+
+
+
+public class ActiuneListaFiltrare implements ActionListener{
+
+	
+	public static ArrayList<Speaker> listaFiltrataSpeaker;
+	
+
+
+	public void actionPerformed(ActionEvent e) {
+          
+
+	  
+	}
+
+}
