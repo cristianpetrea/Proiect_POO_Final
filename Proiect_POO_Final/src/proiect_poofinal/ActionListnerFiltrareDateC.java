96c4aad573dfb6fb0d5b920c474fffdc37aad710
Elisei Avram <ea169@student.ugal.ro>
2023-01-20 08:13:32 +0200

    final

---
+++ Proiect_POO_Final/src/proiect_poofinal/ActionListnerFiltrareDateC.java
@@ -0,0 +1,14 @@
+package proiect_poofinal;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class ActionListnerFiltrareDateC implements ActionListener {
+
+    public static ArrayList<Casti> listaCastiF;
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
--- Proiect_POO_Final/src/proiect_poofinal/Gadget.java
+++ Proiect_POO_Final/src/proiect_poofinal/Gadget.java
@@ -1,42 +1,43 @@
 package proiect_poofinal;
 
-public class Gadget implements Electronice 
-{
-    
-public void AfisarePret(){
-    System.out.println("Pretul este: ");
-}
-public void AfisareStoc(){
-    System.out.println("Stocul este");
-}
-  
+public class Gadget implements Electronice {
+
+    public void AfisarePret() {
+        System.out.println("Pretul este: ");
+    }
+
+    public void AfisareStoc() {
+        System.out.println("Stocul este");
+    }
+
     String brand, culoare, stare;
     float pret;
     int stoc, garantie;
+
     public Gadget() {
-        brand=null;
-        culoare=null;
-        stare=null;
-        garantie=0;
-        pret=0.0f;
-        stoc=0;
-        
-    }
-    public Gadget(String brand,String culoare,String stare,int garantie, float pret, int stoc) {
-        this.brand=brand;
-        this.culoare=culoare;
-        this.stare=stare;
-        this.garantie=garantie;
-        this.pret=pret;
-        this.stoc=stoc;
+        brand = null;
+        culoare = null;
+        stare = null;
+        garantie = 0;
+        pret = 0.0f;
+        stoc = 0;
+
     }
 
-   
+    public Gadget(String brand, String culoare, String stare, int garantie, float pret, int stoc) {
+        this.brand = brand;
+        this.culoare = culoare;
+        this.stare = stare;
+        this.garantie = garantie;
+        this.pret = pret;
+        this.stoc = stoc;
+    }
 
     @Override
     public String toString() {
-        return "Brandul               " + brand + "\n"+"culoarea              " + culoare + "\n"+"starea produsului     " + stare+"\n"
-                + "pretul de             " + pret + " lei \n" + "garantie de           " + garantie + " ani \n";
+        return "Brandul               " + brand + "\n" + "culoarea              " + culoare + "\n"
+                + "starea produsului     " + stare + "\n"
+                + "pretul de             " + pret + " lei \n" + "garantie de           " + garantie + " ani ";
     }
 
 }
